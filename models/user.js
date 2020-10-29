10982311ae793dcccba507e78b17d1f6daf13715
Lexirem <me.vallribera@gmail.com>
2020-10-29 19:48:03 +0100

    mejoras

--- models/user.js
+++ models/user.js
@@ -5,20 +5,12 @@ const userSchema = new Schema({
   name: String,
   email: String,
   password: String,
-<<<<<<< HEAD
-  
-=======
   isLaunderer: { type: Boolean, default: false },
   fee: { type: Number, default: null }
->>>>>>> b11952c28e8ad56cc6499def92c4954aab1e2e61
 });
 
 userSchema.set('timestamps', true);
 
 const User = mongoose.model('User', userSchema);
 
-<<<<<<< HEAD
 module.exports = User;
-=======
-module.exports = User;
->>>>>>> b11952c28e8ad56cc6499def92c4954aab1e2e61
