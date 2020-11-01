68d9f41a781dbc9a1b4ffeaacc6c8804a26d60b6
Lexirem <me.vallribera@gmail.com>
2020-11-01 19:34:02 +0100

    añadimos carrusel

--- routes/photo.js
+++ routes/photo.js
@@ -10,27 +10,27 @@ router.get('/photo', (req, res, next) => {
 
 //indicar ruta de themes y oneTheme o crear theme file en routes
 router.get('/themes', (req, res, next) => {
-    res.render('/photos/themes')
+    res.render('photos/themes')
 });
 
 router.get('/oneTheme', (req, res, next) => {
-    res.render('/photos/oneTheme')
+    res.render('photos/oneTheme')
 });
 
 //indicar ruta de cart
 router.get('/cart', (req, res, next) => {
-    res.render('/photos/cart')
+    res.render('photos/cart')
 });
 
 //indicar ruta de payments
     //ruta get y ruta post
-router.post('/payments', withAuth, async (req, res, next) => {
+router.post('/payments', async (req, res, next) => {
     const userId = req.userID;
     try{
         if(user !== null){
             
         }
-        res.redirect('/photos/message')
+        res.redirect('photos/message')
     }
     catch (error) {
         next(err);
@@ -41,12 +41,12 @@ router.post('/payments', withAuth, async (req, res, next) => {
 
 //indicar ruta a user o podria ir en routes/index.js
     //ruta get y ruta post
-router.get('/user', (req, res, next) => {
-    res.render('/photos/user')
-});
+// router.get('/user', (req, res, next) => {
+//     res.render('photos/user')
+// });
 
-router.post('/user', async (req, res, next) => {
-    const userId = req.userID; 
-})
+// router.post('/user', async (req, res, next) => {
+//     const userId = req.userID; 
+// })
 
 module.exports = router; 
