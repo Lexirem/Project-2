d78833668291a7a759b8fae933195be50a8e2104
Lexirem <me.vallribera@gmail.com>
2020-11-01 19:14:00 +0100

    routes marcadas y parte css

--- routes/photo.js
+++ routes/photo.js
@@ -13,11 +13,40 @@ router.get('/themes', (req, res, next) => {
     res.render('/photos/themes')
 });
 
+router.get('/oneTheme', (req, res, next) => {
+    res.render('/photos/oneTheme')
+});
+
 //indicar ruta de cart
+router.get('/cart', (req, res, next) => {
+    res.render('/photos/cart')
+});
 
 //indicar ruta de payments
     //ruta get y ruta post
+router.post('/payments', withAuth, async (req, res, next) => {
+    const userId = req.userID;
+    try{
+        if(user !== null){
+            
+        }
+        res.redirect('/photos/message')
+    }
+    catch (error) {
+        next(err);
+        return;
+    }
+})
+
+
 //indicar ruta a user o podria ir en routes/index.js
     //ruta get y ruta post
+router.get('/user', (req, res, next) => {
+    res.render('/photos/user')
+});
+
+router.post('/user', async (req, res, next) => {
+    const userId = req.userID; 
+})
 
 module.exports = router; 
