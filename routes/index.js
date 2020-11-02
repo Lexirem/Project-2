c31d46682649f6b33cacd5681ae8b2f3c6e75cb4
Lexirem <me.vallribera@gmail.com>
2020-11-02 17:54:30 +0100

    up to date

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,6 @@ router.get('/', withAuth, function(req, res, next) {
   res.render('index', { title: 'SM-Photography' });
 });
 
- /*username*/
-/*app.get("/user/:username", (req, res, next) => {
-  res.send(req.params);
-});
-
-/* route for cart. */
-// router.get("/cart", (req, res, next) =>{
-//   res.render("photos/cart");
-// }); 
-
-/*
-app.get("/search",  (req, res, next) => {
-  let image = req.query.image
-  res.send(`$(image)`);
-});)
-*/
-
 
 
 
